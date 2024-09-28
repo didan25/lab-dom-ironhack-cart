e6a5bee3c564b98fbc5676e28ddbbd5cfdb39b15
Ana Jerkovic <ajerkovic31@yahoo.com>
2024-09-28 22:33:04 +0200

    Solved lab

--- src/index.js
+++ src/index.js
@@ -3,21 +3,43 @@
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
 
-  //... your code goes here
+  const price = product.querySelector('.price span').textContent;
+  const quantity = product.querySelector('.quantity input').value;
+
+  const priceValue = parseInt(price);
+  const quantityValue = parseInt(quantity);
+
+  const subtotal = priceValue * quantityValue;
+  const subtotalElement = product.querySelector('.subtotal span');
+  subtotalElement.textContent = subtotal.toFixed(2);
+
+  return subtotal;
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
+
   const singleProduct = document.querySelector('.product');
   updateSubtotal(singleProduct);
   // end of test
 
   // ITERATION 2
   //... your code goes here
+  // Get all the product rows
+  const allProducts = document.getElementsByClassName('product');
+
+  let total = 0; 
+
+  for (let i = 0; i < allProducts.length; i++) {
+    total = updateSubtotal(allProducts[i]); 
+  }
+
 
   // ITERATION 3
-  //... your code goes here
+  // Update the total value in the DOM
+  const totalValueElement = document.querySelector('#total-value span');
+  totalValueElement.innerText = total; 
 }
 
 // ITERATION 4
@@ -31,12 +53,42 @@ function removeProduct(event) {
 // ITERATION 5
 
 function createProduct() {
-  //... your code goes here
+  const nameInput = document.querySelector('.create-product input[type="text"]');
+  const priceInput = document.querySelector('.create-product input[type="number"]');
+
+  const name = nameInput.value;
+  const price = parseInt(priceInput.value);
+
+  if (name === "" || price <= 0) {
+    alert("Please provide valid product details.");
+    return;
+  }
+
+  // Create a new row for the product
+  const newProductRow = `
+    <tr class="product">
+      <td class="name">
+        <span>${name}</span>
+      </td>
+      <td class="price">$<span>${price}</span></td>
+      <td class="quantity">
+        <input type="number" value="0" min="0" placeholder="Quantity" />
+      </td>
+      <td class="subtotal">$<span>0</span></td>
+      <td class="action">
+        <button class="btn btn-remove">Remove</button>
+      </td>
+    </tr>`;
+
+  // Insert the new product row into the table
+  const tableBody = document.querySelector('#cart tbody');
+  tableBody.insertAdjacentHTML('beforeend', newProductRow);
 }
+const createProductButton = document.getElementById('create');
+createProductButton.addEventListener('click', createProduct);
+
 
 window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
-
-  //... your code goes here
 });
